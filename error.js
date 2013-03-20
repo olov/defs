c603f9a4b5022e169644da92c7f93719df07cd34
Olov Lassus <olov.lassus@gmail.com>
2013-03-20 11:58:17 +0100

    error.js

---
+++ error.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const fmt = require("./lib/fmt");
+const assert = require("assert");
+
+function error(line, var_args) {
+    assert(arguments.length >= 2);
+
+    const msg = (arguments.length === 2 ?
+        String(var_args) : fmt.apply(fmt, Array.prototype.slice.call(arguments, 1)));
+
+    console.log(fmt("line {0}: {1}", line, msg));
+    error.any = true;
+}
+error.any = false;
+
+module.exports = error;
