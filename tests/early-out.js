b0a6212dcb3ebcfbdddec2276d4399715e59464b
Olov Lassus <olov.lassus@gmail.com>
2013-04-09 16:28:44 +0200

    moved test-input.js to tests/early.js

---
+++ tests/early-out.js
@@ -0,0 +1,16 @@
+"use strict";
+var x;
+function named_fn(a, b) {
+    var unique = 1;
+    for (x = 3; x < 10; x++) {
+        var i = 1;
+        var y$1 = 2;
+        console.log(y$1);
+    }
+    for (var x$2 in [1,2,3,4,5]) {
+        var y$3 = x$2;
+        console.log(y$3);
+    }
+    var y,z;
+}
+named_fn();
