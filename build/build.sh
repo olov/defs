08048ebdffe41630ab61ff490547b84b1d07516e
Olov Lassus <olov.lassus@gmail.com>
2013-07-09 22:47:33 +0200

    move build artefacts to build/es5 and build/browser

--- build/build.sh
+++ build/build.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
+echo "beginning defs self-build"
+rm -rf es5
+mkdir es5
+
 declare -a files=(defs-main.js defs-wrapper.js error.js options.js run-tests.js scope.js stats.js traverse.js)
 for i in ${files[@]}
 do
   echo "building $i"
-  defs ../$i > $i
+  ../defs-harmony ../$i > es5/$i
 done
 
-declare -a cops=(node_modules jshint_globals tests)
-for i in ${cops[@]}
+cp defs es5/
+
+cd es5
+
+declare -a symlinks=(node_modules jshint_globals tests)
+for i in ${symlinks[@]}
 do
-  echo "copying $i"
-  cp -r ../$i .
+  echo "symlinking $i"
+  ln -s ../../$i $i
 done
 
-echo "running tests (without --harmony)"
-NODE=`which node`
-$NODE run-tests.js es5
-echo "done"
+echo "running tests (in es5 mode i.e. without --harmony)"
+/usr/bin/env node run-tests.js es5
+echo "done self-build"
--- build/clean.sh
+++ build/clean.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 echo "cleaning build files"
-rm -f *.js
-declare -a cops=(node_modules jshint_globals tests)
-for i in ${cops[@]}
-do
-  rm -rf $i
-done
-echo "done"
+rm -rf es5 browser
+echo "done cleaning"
